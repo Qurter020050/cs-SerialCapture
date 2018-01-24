8bda2e206a6c35fa732b4dfa4453e03088fa5e92
Kun Huang <qurter@outlook.com>
2018-01-24 23:23:19 +0800

    sync 1801242323

--- SerialCapture/Display/BasicDisplay/ReceiveCode.xaml.cs
+++ SerialCapture/Display/BasicDisplay/ReceiveCode.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace SerialCapture.Display
 {
     /// <summary>
-    /// SerialPortControl.xaml 的交互逻辑
+    /// UserControl1.xaml 的交互逻辑
     /// </summary>
-    public partial class SerialPortControl : UserControl
+    public partial class ReceiveCode : ContentControl
     {
-        public SerialPortControl()
+        public ReceiveCode()
         {
             InitializeComponent();
         }
