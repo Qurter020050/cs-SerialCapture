18585f50345719c071b3a6e75c479ff1553349b7
Kun Huang <hsjtsoft@126.com>
2018-01-24 21:56:34 +0800

    sync 1801242155

--- SerialCapture/Display/MainDisplay.cs
+++ SerialCapture/Display/MainDisplay.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SerialCapture
+namespace SerialCapture.Display
 {
-    public class Display
+    public class MainDisplay
     {
 
     }
--- SerialCapture/Display/StatesDisplay.cs
+++ SerialCapture/Display/StatesDisplay.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SerialCapture
+namespace SerialCapture.Display
 {
     public class StatesDisplay
     {
