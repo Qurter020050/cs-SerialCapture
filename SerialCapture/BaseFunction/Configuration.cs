a8affb3a1d93d5d5565d36a420bcf0e6455e76c9
Kun Huang <hsjtsoft@126.com>
2018-01-24 10:19:09 +0800

    架构建立

---
+++ SerialCapture/BaseFunction/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SerialCapture
+{
+    public static class Config
+    {
+        static Configuration config { get; set; } = new Configuration();
+
+        static Config()
+        {
+
+        }
+    }
+
+    class Configuration
+    {
+
+    }
+
+    class XMLHelper
+    {
+
+    }
+}
